203e25671aa584430e59a3f4be15cce7dfe5e1ba
Wojtek Mach <wojtek@wojtekmach.pl>
2017-04-12 20:14:04 +0200

    Prepare for docs

--- mix.exs
+++ mix.exs
@@ -1,15 +1,26 @@
 defmodule PublishPlus.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :publish_plus,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
-      package: package()
+      package: package(),
+
+      # Docs
+      name: "PublishPlus",
+      docs: [
+        source_ref: "v#{@version}",
+        source_url: "https://github.com/wojtekmach/publish_plus",
+        main: "readme",
+        extras: ["README.md"]
+      ]
     ]
   end
 
