1305698661b7ef22ea0ae2b7beb1db4afb6d4bce
Wojtek Mach <wojtek@wojtekmach.pl>
2017-04-12 19:06:50 +0200

    Add publish_plus mix task

---
+++ lib/mix/tasks/publish_plus.ex
@@ -0,0 +1,85 @@
+defmodule Mix.Tasks.PublishPlus do
+  use Mix.Task
+
+  @shortdoc "Publishes a new package version"
+
+  @moduledoc """
+  PublishPlus is an opinionated package publisher for Elixir.
+
+      mix publish_plus VERSION
+
+  It assumes the following:
+
+  - as usual, packages are published to Hex.pm and docs to HexDocs.pm
+  - for each release, a tag is created and pushed to git
+  - CHANGELOG.md is present and updated with the release
+  - LICENSE.md is present
+
+  A `--dry-run` option might be given to not create and push tags or the package to Hex.
+  """
+
+  @changelog "CHANGELOG.md"
+  @license "LICENSE.md"
+
+  @switches [dry_run: :boolean]
+
+  def run(args) do
+    case OptionParser.parse(args, strict: @switches) do
+      {opts, [version], []} ->
+        project = Mix.Project.config()
+        version = normalize_version(project, version)
+
+        check_changelog(version)
+        check_license()
+
+        unless opts[:dry_run] do
+          publish()
+          create_and_push_tag(version)
+        end
+      _ ->
+        Mix.raise "Usage: mix publish_plus VERSION"
+    end
+  end
+
+  defp normalize_version(project, "v" <> rest), do: normalize_version(project, rest)
+  defp normalize_version(project, version) do
+    check_version(version, project[:version])
+    "v#{version}"
+  end
+
+  def check_version(version, project_version) do
+    if version != project_version do
+      Mix.raise "Expected #{inspect version} to match mix.exs version #{inspect project_version}"
+    end
+  end
+
+  defp check_changelog(version) do
+    unless File.exists?(@changelog) do
+      Mix.raise "#{@changelog} is missing"
+    end
+
+    unless File.read!(@changelog) |> String.contains?(version) do
+      Mix.raise "#{@changelog} does not include an entry for #{version}"
+    end
+  end
+
+  defp check_license do
+    unless File.exists?(@license) do
+      Mix.raise "#{@license} is missing"
+    end
+  end
+
+  defp create_and_push_tag(version) do
+    Mix.shell.info "Creating tag #{version}..."
+    {_, 0} = System.cmd("git", ["tag", version, "-a", "-m", version])
+    Mix.shell.info "done\n"
+
+    Mix.shell.info "Pushing tag #{version}..."
+    {_, 0} = System.cmd("git", ["push", "origin", version])
+    Mix.shell.info "done\n"
+  end
+
+  defp publish do
+    Mix.Tasks.Hex.Publish.run([])
+  end
+end
--- mix.exs
+++ mix.exs
@@ -2,19 +2,35 @@ defmodule PublishPlus.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :publish_plus,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :publish_plus,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      package: package()
+    ]
   end
 
   def application do
-    [applications: [:logger]]
+    [
+      applications: [:logger]
+    ]
   end
 
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  def package do
+    [
+      description: "PublishPlus is an opinionated package publisher for Elixir",
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/wojtekmach/publish_plus"},
+      maintainers: ["Wojtek Mach"],
+    ]
   end
 end
